48ce166eaa3085e0597295f776ffbd798e14c7b3
Richard Jhony Duran Sullcamani <duransullcamanirichard@gmail.com>
2022-08-02 19:24:31 -0400

    Adicion docs

---
+++ docs/precache-manifest.475e25ec7fae737e96257ced21d50764.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1516901446b4a1fda625bca7fb3df16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bdf413854922c27ce84",
+    "url": "/static/css/main.f2354c4f.chunk.css"
+  },
+  {
+    "revision": "2384b4fdb7f98e42af6c",
+    "url": "/static/js/2.5bf6791a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5bf6791a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdf413854922c27ce84",
+    "url": "/static/js/main.94a77509.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.94a77509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0963fe691459a95666",
+    "url": "/static/js/runtime-main.5ec8b05a.js"
+  }
+]);
